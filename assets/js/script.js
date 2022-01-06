0df013a053b4d3b333a81e8de3a4b237ef40a6ec
Ken Libuit <85600631+Keeeenny@users.noreply.github.com>
2022-01-06 07:09:26 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const counter = document.getElementById("counter");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+plus.addEventListener("mousedown", calculator);
+minus.addEventListener("click", calculator);
+
+
+
+function calculator(){
+
+    let identifier = this.attributes["data-value"].value;
+
+    let value = counter.innerHTML;
+
+    let operators = {
+        add: +1,
+        sub: -1,
+    }
+
+    let result = parseInt(value)+operators[identifier];
+
+    document.getElementById("counter").innerHTML = result
+
+
+};
+
+
